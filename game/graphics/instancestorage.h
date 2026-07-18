a8f71dac1ae239a8c7a60220ee9427c4c1410ce1
Patrick Baran <patrick.baran@ewistore.co.uk>
2026-07-18 17:17:56 +0100

    fix(ios): remove ineffective scalar qualifiers

--- game/graphics/instancestorage.h
+++ game/graphics/instancestorage.h
@@ -30,13 +30,13 @@ class InstanceStorage {
         Id& operator = (Id&& other) noexcept;
         ~Id();
 
-        const size_t   size() const { return rgn.asize; }
+        size_t         size() const { return rgn.asize; }
         void           set(const Tempest::Matrix4x4* anim);
         void           set(const Tempest::Matrix4x4& obj, size_t offset);
         void           set(const void* data, size_t offset, size_t size);
 
         template<class T>
-        const uint32_t offsetId() const { return uint32_t(rgn.begin/sizeof(T)); }
+        uint32_t       offsetId() const { return uint32_t(rgn.begin/sizeof(T)); }
 
         bool           isEmpty() const { return rgn.asize==0; }
 
